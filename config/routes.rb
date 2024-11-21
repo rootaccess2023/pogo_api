62828aee442203b9f592d99aaf7bdb83f0aeb49c
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-21 22:54:07 +0000

    feat(routes): add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :locations, only: [ :index ]
+  
+  # Define root route (directing to the locations index action)
+  root 'locations#index'
+  
+  resources :locations, only: [:index]
 end
